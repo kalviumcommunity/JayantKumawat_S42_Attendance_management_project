96949f69cb0efa148f79a287e9a2e5c314ebdced
Jayantx07 <jayantkumawat107@gmail.com>
2025-09-05 11:27:31 +0530

    feat: Define Student and Course classes with array usage (Part 2)

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,6 +2,26 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Attendance Management System!");
+        // Array of Students
+        Student[] students = new Student[2];
+        students[0] = new Student(1, "Jayant", 20);
+        students[1] = new Student(2, "Aman", 21);
+
+        // Array of Courses
+        Course[] courses = new Course[2];
+        courses[0] = new Course("CSE101", "Object Oriented Programming", 4);
+        courses[1] = new Course("CSE102", "Data Structures", 3);
+
+        // Display Students
+        System.out.println("---- Students ----");
+        for (Student s : students) {
+            s.displayStudent();
+        }
+
+        // Display Courses
+        System.out.println("\n---- Courses ----");
+        for (Course c : courses) {
+            c.displayCourse();
+        }
     }
 }
